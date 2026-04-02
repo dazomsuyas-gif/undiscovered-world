350105cd542f61666a0432dc77c0f6c32261ccee
dazomsuyas-gif <dazomsuyas@gmail.com>
2026-04-02 15:44:32 +0300

    Add Header component

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Welcome to the Undiscovered World</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
